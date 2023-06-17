e3be05189a74325ebee223ff5cc5e1f9f5f84c8b
laura <106522200+505laura@users.noreply.github.com>
2023-06-18 05:49:37 +1000

    Edit contact form styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,57 +2,45 @@ import React, { useState, useEffect } from "react";
 
 function Contact() {
   return (
-    <div className="flex min-h-screen items-center justify-start text-offWhite">
-      <div className="mx-auto w-full max-w-lg">
+    <div className="flex pt-16 mx-auto text-offWhite w-3/4">
+      <div className="mx-auto w-3/4">
         <h1 className="text-4xl font-medium">Contact me</h1>
-        <p className="mt-3">Email me at laura@505laura.com or message me here:</p>
+        {/* Make email clickable */}
+        <p className="mt-3">Send me an email at laura@505laura.com or message me here:</p>
 
-        <form action="https://api.web3forms.com/submit" className="mt-10">
-          <input type="hidden" name="access_key" value="YOUR_ACCESS_KEY_HERE" />
+        <form className="mt-10">
           <div className="grid gap-6 sm:grid-cols-2">
             <div className="relative z-0">
-              <input
-                type="text"
-                name="name"
-                className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
-                placeholder=" "
-              />
+              <input type="text" name="name" className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" "/>
               <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">
                 Your name
               </label>
             </div>
             <div className="relative z-0">
-              <input
-                type="text"
-                name="email"
-                className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
-                placeholder=" "
-              />
+              <input type="text" name="email" className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" "/>
               <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">
                 Your email
               </label>
             </div>
             <div className="relative z-0 col-span-2">
-              <textarea
-                name="message"
-                rows="5"
-                className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
-                placeholder=" "
-              ></textarea>
+              <textarea name="message" rows="5" className="peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0" placeholder=" "></textarea>
               <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">
                 Your message
               </label>
             </div>
           </div>
-          <button
-            type="submit"
-            className="mt-5 rounded-md bg-black px-10 py-2 text-white"
-          >
+
+
+          <button type="submit" className="mt-5 rounded-md bg-accent px-10 py-2 text-black">
             Send Message
           </button>
         </form>
       </div>
     </div>
+
+
+// Add socials 
+
   );
 }
 
