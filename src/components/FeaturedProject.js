a729f6cd9888c8d23e4c99927a41ab92626cfe1c
laura <106522200+505laura@users.noreply.github.com>
2023-06-18 20:07:48 +1000

    Implement layout for portfolio page

--- src/components/FeaturedProject.js
+++ src/components/FeaturedProject.js
@@ -2,16 +2,29 @@ import React, { useState, useEffect } from 'react';
 import Cityspire from '../Cityspire.gif';
 import CodeQuiz from '../code-quiz.gif';
 import TechBlog from '../tech-blog.gif';
-import FeaturedText from './FeaturedText';
 
 // Resize gifs so that they are consistent
 
 function FeaturedProject() {
   return (
-    <div>
-      <div>
-        <img src={Cityspire} alt="Featured project"  className='h-40 w-auto'/>
-        <FeaturedText></FeaturedText>
+    <div className='flex pb-10'>
+      <div className='flex max-w-sm'>
+        <img src={Cityspire} alt="Featured project"  className="pr-4"/>
+      </div>
+      <div className='grid content-between'>
+        <div>
+          <h3 className='text-offWhite font-semibold text-lg pb-2'>
+            Travel Website
+          </h3>
+          <p className='text-gray-500'>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+        </div>
+        <div>
+          <p className='text-accent font-semibold text-xs pb-1'>
+            Node.js Express.js Tailwind CSS
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,20 +4,22 @@ import Project from './Project';
 
 function Portfolio() {
   return (
-    <div className='w-2/3 mx-auto'>
-      <h2 className='text-4xl text-offWhite pb-5'>
+    <div className='w-3/4 mx-auto pt-8'>
+      {/* <h2 className='w-3/4 text-4xl text-offWhite pb-5'>
         Portfolio
-      </h2>
+      </h2> */}
       <FeaturedProject></FeaturedProject>
       <FeaturedProject></FeaturedProject>
       <FeaturedProject></FeaturedProject>
       <h3 className='text-2xl text-offWhite py-5'>
         Some other projects
       </h3>
+      <div className="grid grid-cols-4 gap-3">
       <Project></Project>
       <Project></Project>
       <Project></Project>
       <Project></Project>
+      </div>
     </div>
   );
 }
